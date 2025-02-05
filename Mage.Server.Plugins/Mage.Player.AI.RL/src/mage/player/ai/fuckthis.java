52e349742e8c1a6b77dcb8ea407461a95ef1499c
WillWroble <willwroble@gmail.com>
2025-02-04 19:51:43 -0500

    adding modules

---
+++ Mage.Server.Plugins/Mage.Player.AI.RL/src/mage/player/ai/fuckthis.java
@@ -0,0 +1,4 @@
+package mage.player.ai;
+import mage.MageObject;
+public class fuckthis {
+}
---
+++ Mage.Tests/src/test/java/org/mage/test/AI/basic/RLAIvsAITest.java
@@ -0,0 +1,76 @@
+package org.mage.test.AI.basic;
+
+import mage.constants.PhaseStep;
+import mage.constants.RangeOfInfluence;
+import mage.constants.Zone;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mage.test.player.TestComputerPlayer7;
+import org.mage.test.player.TestPlayer;
+import org.mage.test.serverside.base.CardTestPlayerBaseAI;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author JayDi85
+ */
+public class RLAIvsAITest extends CardTestPlayerBaseAI {
+
+    @Override
+    public List<String> getFullSimulatedPlayers() {
+        return Arrays.asList("PlayerA", "PlayerB");
+    }
+
+    @Test
+    public void test_AIvsAI_Simple() {
+        // both must kill x2 bears by x2 bolts
+        addCard(Zone.BATTLEFIELD, playerA, "Balduvian Bears", 2);
+        addCard(Zone.BATTLEFIELD, playerB, "Balduvian Bears", 2);
+        addCard(Zone.HAND, playerA, "Lightning Bolt", 2);
+        addCard(Zone.HAND, playerB, "Lightning Bolt", 2);
+        addCard(Zone.BATTLEFIELD, playerA, "Mountain", 2);
+        addCard(Zone.BATTLEFIELD, playerB, "Mountain", 2);
+
+        setStrictChooseMode(true);
+        setStopAt(1, PhaseStep.END_TURN);
+        execute();
+
+        assertGraveyardCount(playerA, "Balduvian Bears", 2);
+        assertGraveyardCount(playerB, "Balduvian Bears", 2);
+    }
+
+    @Test
+    public void test_AIvsAI_LongGame() {
+        // many bears and bolts must help to end game fast
+        int maxTurn = 50;
+        removeAllCardsFromLibrary(playerA);
+        removeAllCardsFromLibrary(playerB);
+
+        addCard(Zone.LIBRARY, playerA, "Mountain", 10);
+        addCard(Zone.LIBRARY, playerA, "Forest", 10);
+        addCard(Zone.LIBRARY, playerA, "Lightning Bolt", 20);
+        addCard(Zone.LIBRARY, playerA, "Balduvian Bears", 10);
+        //
+        addCard(Zone.LIBRARY, playerB, "Mountain", 10);
+        addCard(Zone.LIBRARY, playerA, "Forest", 10);
+        addCard(Zone.LIBRARY, playerB, "Lightning Bolt", 20);
+        addCard(Zone.LIBRARY, playerB, "Balduvian Bears", 10);
+
+        // full ai simulation
+        //setStrictChooseMode(true);
+        setStopAt(maxTurn, PhaseStep.END_TURN);
+        execute();
+
+        Assert.assertTrue("One of player must won a game before turn " + maxTurn + ", but it ends on " + currentGame, currentGame.hasEnded());
+    }
+    @Override
+    protected TestPlayer createPlayer(String name, RangeOfInfluence rangeOfInfluence) {
+        if (getFullSimulatedPlayers().contains(name)) {
+            TestPlayer testPlayer = new TestPlayer(new TestComputerPlayer7(name, RangeOfInfluence.ONE, getSkillLevel()));
+            testPlayer.setAIPlayer(true); // enable full AI support (game simulations) for all turns by default
+            return testPlayer;
+        }
+        return super.createPlayer(name, rangeOfInfluence);
+    }
+}
